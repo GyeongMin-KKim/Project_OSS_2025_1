a9ae941619153da1113526abf1f08eab2359c25b
GyeongMin Kim <tomny25@yu.ac.kr>
2025-06-04 20:45:25 +0900

    [기능추가] 파스칼(Pa),핵토파스칼(hPa) 변환 기능 추가

--- OSS_Calc/calc.py
+++ OSS_Calc/calc.py
@@ -1,25 +1,22 @@
 import tkinter as tk
 
-
 class Calculator:
     def __init__(self, root):
         self.root = root
         self.root.title("계산기")
-        self.root.geometry("300x400")
+        self.root.geometry("300x450")
 
         self.expression = ""
 
-        # 입력창
         self.entry = tk.Entry(root, font=("Arial", 24), justify="right")
         self.entry.pack(fill="both", ipadx=8, ipady=15, padx=10, pady=10)
 
-        # 버튼 생성
         buttons = [
             ['7', '8', '9', '/'],
             ['4', '5', '6', '*'],
             ['1', '2', '3', '-'],
             ['0', '.', 'C', '+'],
-            ['=']
+            ['hPa→Pa', 'Pa→hPa', '=']
         ]
 
         for row in buttons:
@@ -37,16 +34,29 @@ class Calculator:
     def on_click(self, char):
         if char == 'C':
             self.expression = ""
+
         elif char == '=':
             try:
                 self.expression = str(eval(self.expression))
             except Exception:
                 self.expression = "에러"
+
+        elif char == 'hPa→Pa':
+            try:
+                value = float(self.expression)
+                self.expression = str(value * 100)
+            except:
+                self.expression = "에러"
+
+        elif char == 'Pa→hPa':
+            try:
+                value = float(self.expression)
+                self.expression = str(value / 100)
+            except:
+                self.expression = "에러"
+
         else:
             self.expression += str(char)
 
         self.entry.delete(0, tk.END)
         self.entry.insert(tk.END, self.expression)
-
-
-
